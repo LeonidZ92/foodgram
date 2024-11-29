96b9a0cfcb4a7050907d4efa8bb029353111f252
Leonid Zykov <leonidzykov999@gmail.com>
2024-11-29 17:13:34 +0300

    add utils.py

--- backend/api/views.py
+++ backend/api/views.py
@@ -8,7 +8,6 @@ from django_filters.rest_framework import DjangoFilterBackend
 from djoser.views import UserViewSet
 from rest_framework import status, viewsets
 from rest_framework.decorators import action
-from rest_framework.exceptions import NotAuthenticated
 from rest_framework.permissions import (
     AllowAny,
     IsAuthenticated,
